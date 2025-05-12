1aa2b4ac6188a98d5be232085a2fec95b5179f33
Laurens Moonens <laurens32.moonens@telenet.be>
2025-05-12 23:45:43 +0200

    Rainbow square and flickering square

---
+++ Engine/src/Utils/FileIO.c
@@ -0,0 +1,33 @@
+#include "FileIO.h"
+#include "Logger.h"
+
+#include <stdio.h>
+
+
+char* GetStringFromFile(const char* filePath)
+{
+    FILE* file = fopen(filePath, "rb");
+
+    if(file == NULL)
+    {
+        LogError("Unable to open file at path %s", filePath);
+        return NULL;
+    }
+
+    fseek(file, 0L, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+
+    char* result = (char*)malloc(length + 1);
+    if(result == NULL)
+    {
+        LogError("Memory allocation failed for file %s", filePath);
+        return NULL;
+    }
+
+    fread(result, sizeof(char), length, file);
+    result[length] = '\0';
+    fclose(file);
+
+    return result;
+}
--- TestProject/fragmentShader.glsl
+++ TestProject/fragmentShader.glsl
@@ -1,8 +1,10 @@
-#version 150 core
+#version 330 core
 
 out vec4 outColor;
 
+uniform vec4 customColor;
+
 void main()
 {
-    outColor = vec4(1.0, 0.0, 1.0, 1.0);
+    outColor = customColor;
 }
