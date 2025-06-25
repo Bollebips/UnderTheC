3255baf621fdeff4f30bb95f3afe9be9b02aa1ca
Laurens Moonens <laurens32.moonens@telenet.be>
2025-06-26 00:50:43 +0200

    WIP traversing octree

--- Demo/resources/voxelShader.glsl
+++ Demo/resources/voxelShader.glsl
@@ -14,20 +14,22 @@ const int maxSteps = 32;
 
 const int voxelBrickSize = 4;
 const float cubeWidth = 1.0f / voxelBrickSize;
-const uint64_t voxelBrick = 0x5A5A000F88FFFFFFul;
-//0101 1010 0101 1010
+const uint64_t voxelBrick_OLD = 0x505A000F88FFFFFFul;
+//0101 0000 0101 1010
 //0000 0000 0000 1111
 //1000 1000 1111 1111
 //1111 1111 1111 1111
 
 // 1
 //1 1 1 1 1 1 1 1
+                            
+// 2  2   0  0   2  2   D  D 
+// 01 01  00 00  01 01  10 10
+// 00 00  00 00  00 00  11 11
 
-//0101 1010 0101 1010
-//0000 0000 0000 1111
-
-//1000 1000 1111 1111
-//1111 1111 1111 1111
+// D  C   D  C   F  F   F  F
+// 10 00  10 00  11 11  11 11
+// 11 11  11 11  11 11  11 11
 
 layout (location = 2) uniform uint octree[9];
 
@@ -45,9 +47,17 @@ struct Ray
     vec3 Direction;
 };
 
-struct RayDestination
+struct DDA
 {
-    bool Hit;
+    vec3 NormalizedOrigin;
+    vec3 InitialSideDistace;
+    vec3 CellDeltaDistance;
+    vec3 TotalDeltaDistance;
+    ivec3 CellStep;
+    ivec3 Chunk;
+    ivec3 CurrentStepDirection;
+    float CellSize;
+    float TotalDistance;
 };
 
 struct VoxelBrick
@@ -55,92 +65,194 @@ struct VoxelBrick
     uint Occupancy;
     uint AbsoluteIndex;
     uint Level;
-    float Size;
     vec3 Center;
+    // TODO: Specify max tree depth
+    uint ParentIndeces[5];
 };
 
-bool IsVoxelHit(VoxelBrick voxelBrick, vec3 hitPosition)
+struct VoxelHit
 {
-    vec3 voxelHitPercentage = mod(hitPosition, voxelBrick.Size) / voxelBrick.Size; // [0, voxelBrick.Size]
-    ivec3 voxelAxisIndex = ivec3(ceil(voxelHitPercentage - 0.5f)); // [0, 1]
+    bool IsHit;
+    uvec3 HitVoxelIndex;
+    uint VoxelIndex;
+    vec3 HitNormal;
+};
 
-    int voxelIndex = voxelAxisIndex.x + voxelAxisIndex.y * 2 + voxelAxisIndex.z * 4;
-    return ((1 << 0) & octree[voxelBrick.AbsoluteIndex]) != 0;
+void StepLevelDownOctree(inout DDA dda, inout VoxelBrick voxelBrick, VoxelHit hit)
+{
+    uint nrOfSiblingsToSkip = 0;
+    uint occupancyMask = voxelBrick.Occupancy;
+    occupancyMask >>= hit.VoxelIndex;
+    while (occupancyMask > 0)
+    {
+        occupancyMask &= (occupancyMask - 1);
+        nrOfSiblingsToSkip++;
+    }
+
+    voxelBrick.ParentIndeces[voxelBrick.Level] = voxelBrick.AbsoluteIndex;
+    voxelBrick.AbsoluteIndex += nrOfSiblingsToSkip;
+    voxelBrick.Level++;
+    dda.CellSize = pow(0.5f, voxelBrick.Level);
+    voxelBrick.Center += dda.CellSize * (vec3(hit.HitVoxelIndex) - 0.5f); 
 }
 
-RayDestination StepDDA(const Ray ray, float maxDistance)
+void StepLevelUpOctree(inout DDA dda, inout VoxelBrick voxelBrick, VoxelHit hit)
 {
-    //TODO: scale with different cell size
+    voxelBrick.AbsoluteIndex = voxelBrick.ParentIndeces[voxelBrick.Level];
+    voxelBrick.Level--;
+    dda.CellSize = pow(0.5f, voxelBrick.Level);
+    voxelBrick.Center -= dda.CellSize * (vec3(hit.HitVoxelIndex) - 0.5f); 
+}
 
-    float cellSize = 1f;
+VoxelHit CheckVoxelHit(const VoxelBrick voxelBrick, const vec3 hitPosition, const ivec3 stepDirection)
+{
+    float voxelSize = pow(0.5f, voxelBrick.Level);
+
+    uvec3 voxelAxisIndex = uvec3(ceil(hitPosition - voxelBrick.Center));
+
+    uint voxelIndex = voxelAxisIndex.x + voxelAxisIndex.y * 2 + voxelAxisIndex.z * 4;
+    return VoxelHit
+    ( 
+        ((1 << voxelIndex) & octree[voxelBrick.AbsoluteIndex]) != 0,
+        voxelAxisIndex,
+        voxelIndex,
+        stepDirection * -1
+    );
+}
+
+DDA StartDDA(const Ray ray)
+{
+    float chunkSize = 1f;
+    float cellSize = chunkSize;
     int level = 0;
     int minLevel = 1;
 
-    vec3 o = ray.Origin / cellSize;
-
-    float cellSizeSqr = pow(cellSize, 2);
-
+    DDA dda;
+    dda.CellSize = chunkSize;
+    dda.NormalizedOrigin = ray.Origin / cellSize;
     // TODO: handle 0 division
-    vec3 c = abs(1.0f / ray.Direction);
+    dda.CellDeltaDistance = abs(1.0f / ray.Direction);
+    dda.Chunk = ivec3(floor(ray.Origin / chunkSize));
+    dda.CellStep = ivec3(sign(ray.Direction));
 
-    ivec3 cell = ivec3(floor(ray.Origin / cellSize));
-    ivec3 cellStep = ivec3(sign(ray.Direction));
+    dda.InitialSideDistace.x = ray.Direction.x < 0 ?
+        dda.NormalizedOrigin.x - (floor(dda.NormalizedOrigin.x)):
+        (floor(dda.NormalizedOrigin.x) + 1) - dda.NormalizedOrigin.x;
 
-    float ax = ray.Direction.x < 0 ?
-        o.x - (floor(o.x)):
-        (floor(o.x) + 1) - o.x;
+    dda.InitialSideDistace.y = ray.Direction.y < 0 ?
+        dda.NormalizedOrigin.y - (floor(dda.NormalizedOrigin.y)):
+        (floor(dda.NormalizedOrigin.y) + 1) - dda.NormalizedOrigin.y;
 
-    float ay = ray.Direction.y < 0 ?
-        o.y - (floor(o.y)):
-        (floor(o.y) + 1) - o.y;
+    dda.InitialSideDistace.z = ray.Direction.z < 0 ?
+        dda.NormalizedOrigin.z - (floor(dda.NormalizedOrigin.z)):
+        (floor(dda.NormalizedOrigin.z) + 1) - dda.NormalizedOrigin.z;
 
-    float az = ray.Direction.z < 0 ?
-        o.z - (floor(o.z)):
-        (floor(o.z) + 1) - o.z;
+    dda.TotalDeltaDistance = dda.InitialSideDistace * dda.CellDeltaDistance * cellSize;
+    dda.TotalDistance = 0.0f;
 
-    float tx = ax * c.x * cellSize;
-    float ty = ay * c.y * cellSize;
-    float tz = az * c.z * cellSize;
+    return dda;
+}
 
-    float totalDistance = 0.0f;
-
-    RayDestination result = RayDestination(false);
-    VoxelBrick v;
-
-    while(totalDistance < maxDistance && result.Hit == false)
+void StepDDA(inout DDA dda)
+{
+    if(dda.TotalDeltaDistance.x < dda.TotalDeltaDistance.y && dda.TotalDeltaDistance.x < dda.TotalDeltaDistance.z)
     {
-        if(cell == ivec3(0, 0, 0))
-        {
-            v.Occupancy = octree[0];
-            v.AbsoluteIndex = 0;
-            v.Level = 0;
-            v.Size = 1;
-            v.Center = vec3(0.5f);
-            result.Hit = IsVoxelHit(v, ray.Origin + ray.Direction * totalDistance);
-            break;
-        }
+        dda.TotalDistance = dda.TotalDeltaDistance.x;
+        dda.TotalDeltaDistance.x += dda.CellDeltaDistance.x * dda.CellSize;
+        dda.CurrentStepDirection = ivec3(dda.CellStep.x, 0, 0);
+    }
+    else if(dda.TotalDeltaDistance.y < dda.TotalDeltaDistance.x && dda.TotalDeltaDistance.y < dda.TotalDeltaDistance.z)
+    {
+        dda.TotalDistance = dda.TotalDeltaDistance.y;
+        dda.TotalDeltaDistance.y += dda.CellDeltaDistance.y * dda.CellSize;
+        dda.CurrentStepDirection = ivec3(0, dda.CellStep.y, 0);
+    }
+    else
+    {
+        dda.TotalDistance = dda.TotalDeltaDistance.z;
+        dda.TotalDeltaDistance.z += dda.CellDeltaDistance.z * dda.CellSize;
+        dda.CurrentStepDirection = ivec3(0, 0, dda.CellStep.z);
+    }
+}
 
-        if(tx < ty && tx < tz)
+VoxelHit TraverseChunk(inout DDA dda, in VoxelBrick voxelBrick, vec3 hitPos)
+{
+    int maxLevel = 0;
+    VoxelHit hit;
+
+    //TODO: Check if we should step down a level or step up
+            
+    while(voxelBrick.Level >= 0)
+    {
+        hit = CheckVoxelHit(voxelBrick, hitPos, dda.CurrentStepDirection);
+        //find voxel octree level we can traverse
+        if(hit.IsHit)
         {
-            totalDistance = tx;
-            tx += c.x * cellSize;
-            cell.x += cellStep.x;
-        }
-        else if(ty < tx && ty < tz)
-        {
-            totalDistance = ty;
-            ty += c.y * cellSize;
-            cell.y += cellStep.y;
+            if(voxelBrick.Level < maxLevel)
+            {
+                StepLevelDownOctree(dda, voxelBrick, hit);
+            }
+            else
+            {
+                return hit;
+            }
         }
         else
         {
-            totalDistance = tz;
-            tz += c.z * cellSize;
-            cell.z += cellStep.z;
+            StepLevelDownOctree(dda, voxelBrick, hit);
+
+            //Stepping INSIDE the voxel brick
+            uvec3 previousHitVoxelIndex = hit.HitVoxelIndex;
+            StepDDA(dda);
+            uvec3 nextVoxelIndexToCheck = previousHitVoxelIndex + dda.CurrentStepDirection;
+            if(any(lessThan(nextVoxelIndexToCheck, uvec3(0))) || any(greaterThan(nextVoxelIndexToCheck, uvec3(1))))
+            {
+                StepLevelUpOctree(dda, voxelBrick, hit);
+            }
         }
     }
+    
+    return hit;
+}
 
-    vec3 collisionPoint = ray.Origin + ray.Direction * totalDistance;
+bool IsChunkOccupied(const ivec3 chunkIndex)
+{
+    return chunkIndex == ivec3(0);
+}
+
+VoxelHit TraverseDDA(const Ray ray, float maxDistance)
+{
+    DDA dda = StartDDA(ray);
+
+    VoxelHit result = VoxelHit(false, uvec3(0), 0, vec3(0));
+    VoxelBrick voxelBrick;
+
+    while(dda.TotalDistance < maxDistance && result.IsHit == false)
+    {
+        // TODO: Change this to also use CheckVoxelHit instead of a separate chunk function
+        // the voxelBrick should be retrieved from the chunk it's checking
+        if(IsChunkOccupied(dda.Chunk))
+        {
+            //get chunk data
+            voxelBrick.Occupancy = octree[0];
+            voxelBrick.AbsoluteIndex = 0;
+            voxelBrick.Level = 0;
+            voxelBrick.Center = vec3(0.5f);
+
+            vec3 hitPos = ray.Origin + ray.Direction * dda.TotalDistance;
+            result = TraverseChunk(dda, voxelBrick, hitPos);
+
+            if(result.IsHit)
+            {
+                break;
+            }
+        }
+
+        StepDDA(dda);
+        dda.Chunk += dda.CurrentStepDirection;
+    }
+
+    vec3 collisionPoint = ray.Origin + ray.Direction * dda.TotalDistance;
 
     return result;
 }
@@ -186,30 +298,30 @@ void main()
     bool hit = false;
     vec3 hitNormal;
 
-    RayDestination result = StepDDA(ray, farPlaneDistance);
-    if(result.Hit)
+    VoxelHit result = TraverseDDA(ray, farPlaneDistance);
+    if(result.IsHit)
     {
-        imageStore(renderTarget, pixelCoord, vec4(1.0f, 0, 0, 1.0f));
+        imageStore(renderTarget, pixelCoord, vec4(0.5f * (result.HitNormal + 1.0f), 1.0f));
     }
-    else
+    else if(false)
     {
     while(totalDistance < farPlaneDistance)
     {
         float distance = farPlaneDistance;
         vec3 hitCubeCenter = vec3(0.0f);
 
-        for(int i = 0; i < voxelBrickSize * voxelBrickSize * voxelBrickSize; ++i)
+        for(int i = 0; i < voxelBrick_OLD * voxelBrick_OLD * voxelBrick_OLD; ++i)
         {
-            if(((1ul << i) & voxelBrick) == 0)
+            if(((1ul << i) & voxelBrick_OLD) == 0)
             {
                 continue;
             }
 
             uvec3 cubeID = uvec3
             (
-                i % voxelBrickSize,
-                (i / voxelBrickSize) / voxelBrickSize,
-                (i / voxelBrickSize) % voxelBrickSize
+                i % voxelBrick_OLD,
+                (i / voxelBrick_OLD) / voxelBrick_OLD,
+                (i / voxelBrick_OLD) % voxelBrick_OLD
             );
 
             vec3 cubeCenter = vec3(0) + (cubeID * cubeWidth);
