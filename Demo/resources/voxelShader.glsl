4d05e3a615ffbcf9f8eba22c3c0fb04e9158cd00
Laurens Moonens <laurens32.moonens@telenet.be>
2025-11-26 16:14:56 +0100

    Old WIP

--- Demo/resources/voxelShader.glsl
+++ Demo/resources/voxelShader.glsl
@@ -8,26 +8,39 @@ layout (rgba32f, binding = 0) uniform writeonly image2D renderTarget;
 layout(location = 0) uniform mat4 cameraTransform;
 layout(location = 1) uniform vec4 viewportDimensions;
 
+struct ParentVoxel
+{
+    ivec3 voxelAxisIndex;
+    int octreeIndex;
+};
+
+const int maxDepth = 1;
+int currentDepth = 0;
+ParentVoxel[maxDepth] parentStack; // stores the index in the octree array
+int currentChildSlotIndex = 0; // [0-7]
+vec3 currentParentVoxelCenter;
 
 const float collisionDistance = 0.001f;
 const int maxSteps = 32;
 
 const int voxelBrickSize = 4;
 const float cubeWidth = 1.0f / voxelBrickSize;
-const uint64_t voxelBrick = 0x5A5A000F88FFFFFFul;
-//0101 1010 0101 1010
+const uint64_t voxelBrick_OLD = 0x505A000F88FFFFFFul;
+//0101 0000 0101 1010
 //0000 0000 0000 1111
 //1000 1000 1111 1111
 //1111 1111 1111 1111
 
 // 1
-//1 1 1 1 1 1 1 1
+//1 0 1 1 1 1 1 1
 
-//0101 1010 0101 1010
-//0000 0000 0000 1111
+// 2  2   0  0   2  2   D  D
+// 01 01  00 00  01 01  10 10
+// 00 00  00 00  00 00  11 11
 
-//1000 1000 1111 1111
-//1111 1111 1111 1111
+// D  C   D  C   F  F   F  F
+// 10 00  10 00  11 11  11 11
+// 11 11  11 11  11 11  11 11
 
 layout (location = 2) uniform uint octree[9];
 
@@ -48,12 +61,15 @@ struct Ray
 struct RayDestination
 {
     bool Hit;
+    vec4 color;
 };
 
 RayDestination StepDDA(const Ray ray, float maxDistance)
 {
     //TODO: scale with different cell size
 
+    // TODO: consider making the smallest cellSize = 2
+    // -> this way, a some calculations could be limited to ints and binary operations
     float cellSize = 1;
 
     float dx = ray.Direction.x;
@@ -84,42 +100,104 @@ RayDestination StepDDA(const Ray ray, float maxDistance)
         oz - (floor(oz)):
         (floor(oz) + 1) - oz;
 
-    float tx = ax * c.x * cellSize;
-    float ty = ay * c.y * cellSize;
-    float tz = az * c.z * cellSize;
+    float nextTx = ax * c.x * cellSize;
+    float nextTy = ay * c.y * cellSize;
+    float nextTz = az * c.z * cellSize;
+
+    float tx = 0;
+    float ty = 0;
+    float tz = 0;
 
     float totalDistance = 0.0f;
 
-    RayDestination result = RayDestination(false);
+    RayDestination result = RayDestination(false, vec4(0));
+    vec3 hitPos = ray.Origin;
 
     while(totalDistance < maxDistance && result.Hit == false)
     {
+        //chunk hit
         if(cell == ivec3(0, 0, 0))
         {
             result.Hit = true;
+            currentParentVoxelCenter = vec3(cellSize) / 2;
+            uint currentParentVoxelOccupancy = octree[0];
+            vec3 voxelMin = vec3(0);
+            vec3 voxelMax = vec3(1);
+            ivec3 voxelAxisIndex = ivec3(round((hitPos - voxelMin) / (voxelMax - voxelMin)));
+            currentChildSlotIndex = voxelAxisIndex.z + (2 * voxelAxisIndex.y) + (4 * voxelAxisIndex.x);
+            result.color = vec4(0, 0, 0, 1);
+            int colorIndex = 0;
+            result.color[currentDepth] = currentChildSlotIndex / 7.0f;
+
+            bool terminatedChunk = false;
+
+            while(terminatedChunk == false)
+            {
+                if((currentParentVoxelOccupancy & (uint(1) << currentChildSlotIndex)) != 0)
+                {
+                    //PUSH through to child voxel
+                    ivec3 centerMovementMultiplier = (voxelAxisIndex * 2) - 1;
+                    cellSize /= 2.0f;
+                    currentParentVoxelCenter += (cellSize / 2.0f) * centerMovementMultiplier;
+                    voxelMin = currentParentVoxelCenter - vec3(cellSize);
+                    voxelMax = currentParentVoxelCenter + vec3(cellSize);
+
+                    //ivec3 voxelAxisIndex = ivec3(round((hitPos / cellSize) - trunc(hitPos / cellSize)));
+                    // TODO: Find a formula without division? Maybe using sign()?
+                    // TODO: parentStack[currentDepth] = ParentVoxel(voxelAxisIndex, currentChildSlotIndex);
+                    currentDepth += 1;
+                    voxelAxisIndex = ivec3(round((hitPos - voxelMin) / (voxelMax - voxelMin)));
+                    currentChildSlotIndex = voxelAxisIndex.z + (2 * voxelAxisIndex.y) + (4 * voxelAxisIndex.x);
+                    result.color[currentDepth] = currentChildSlotIndex / 7.0f;
+
+                    if(currentDepth <= maxDepth)
+                    {
+                        terminatedChunk = true;
+                    }
+
+                    continue;
+                }
+
+                //ADVANCE to the next sibling voxel
+                // TODO: assume that we travel through the same parent voxel
+                // find which axis to step to, and flip that bit voxelAxisIndex corresponding to that axis
+                // next, check if the flipped bit agrees with the direction of the ray
+                // e.g. if dx > 0, then the bit of the x axis is only allowed to increase, not decrease
+                // if we encounter no conflict, we are allowed to ADVANCE 
+
+                
+
+                terminatedChunk = true;
+            }
+
+            //result.normal = voxelAxisIndex;
+
             break;
         }
 
-        if(tx < ty && tx < tz)
+        if(nextTx < nextTy && nextTx < nextTz)
         {
             totalDistance = tx;
-            tx += c.x * cellSize;
+            tx = nextTx;
+            nextTx += c.x * cellSize;
             cell.x += cellStep.x;
         }
-        else if(ty < tx && ty < tz)
+        else if(nextTy < nextTx && nextTy < nextTz)
         {
             totalDistance = ty;
-            ty += c.y * cellSize;
+            ty = nextTy;
+            nextTy += c.y * cellSize;
             cell.y += cellStep.y;
         }
         else
         {
             totalDistance = tz;
-            tz += c.z * cellSize;
+            tz = nextTz;
+            nextTz += c.z * cellSize;
             cell.z += cellStep.z;
         }
 
-        vec3 point = ray.Origin + ray.Direction * totalDistance;
+        hitPos = ray.Origin + ray.Direction * totalDistance;
         //if(all(greaterThanEqual(point, vec3(0))) && all(lessThanEqual(point, vec3(cellSize))))
         
     }
@@ -173,18 +251,19 @@ void main()
     RayDestination result = StepDDA(ray, farPlaneDistance);
     if(result.Hit)
     {
-        imageStore(renderTarget, pixelCoord, vec4(1.0f, 0, 0, 1.0f));
+        imageStore(renderTarget, pixelCoord, result.color);
     }
     else
     {
-    while(totalDistance < farPlaneDistance)
+    //OLD
+    /*while(totalDistance < farPlaneDistance)
     {
         float distance = farPlaneDistance;
         vec3 hitCubeCenter = vec3(0.0f);
 
         for(int i = 0; i < voxelBrickSize * voxelBrickSize * voxelBrickSize; ++i)
         {
-            if(((1ul << i) & voxelBrick) == 0)
+            if(((1ul << i) & voxelBrick_OLD) == 0)
             {
                 continue;
             }
@@ -235,6 +314,6 @@ void main()
     if(hit && totalDistance > 0)
     {
         imageStore(renderTarget, pixelCoord, vec4(0.5f * (hitNormal + 1.0f), 1.0f));
-    }
+    }*/
     }
 }
